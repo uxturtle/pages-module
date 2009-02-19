66268d5219f3867abd18d499a6e796fb9da68bd5
Sam <sam@samsoff.es>
2009-02-20 00:49:34 +0800

    Added a comment
    Signed-off-by: UX Turtle <github@uxturtle.com>

--- helpers/markdown.php
+++ helpers/markdown.php
@@ -15,6 +15,7 @@ class markdown_Core {
 		return '<div class="markdown">'.$markdown->transform($string).'</div>';
 	}
 	
+	// Easily create links in markdown
 	public static function anchor($uri, $title = NULL, $protocol = NULL)
 	{
 		if ($uri === '')
